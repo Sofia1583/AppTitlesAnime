d9d879ac12984b18846d32f93a96f48b57b27d15
Aleksandra Sycheva <sashasychevaisio@gmail.com>
2025-04-10 12:59:43 +0500

    Добавлены формы для работы со справочником Типы аниме

---
+++ AppTitlesAnime/FormListTypes.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppTitlesAnime
+{
+    public partial class FormListTypes : Form
+    {
+        public FormListTypes()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnAddType_Click(object sender, EventArgs e)
+        {
+            FormAddType formAddType = new FormAddType();
+            formAddType.ShowDialog();
+        }
+    }
+}
